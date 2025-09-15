76ac8b0a40d06c2d9d241a327193e5395c85c0e6
SUHO PARK <duglikespiano@gmail.com>
2025-09-15 16:28:23 +0900

    SETTING ROUTES

---
+++ routes/posts.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+let posts = [
+	{ id: 1, title: 'POST ONE' },
+	{ id: 2, title: 'POST TWO' },
+	{ id: 3, title: 'POST THREE' },
+];
+
+// get all posts
+router.get('/', (req, res) => {
+	const limit = parseInt(req.query.limit);
+
+	if (!isNaN(limit) && limit > 0) {
+		return res.status(200).json(posts.slice(0, limit));
+	}
+	res.status(200).json(posts);
+});
+
+// get a single post
+router.get('/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const post = posts.find((post) => post.id === id);
+	if (!post) {
+		return res.status(404).json({ message: `A post with the id ${id} was not found` });
+	}
+	res.status(200).json(post);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,11 @@
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 1000;
-
+const posts = require('./routes/posts');
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-	{ id: 1, title: 'POST ONE' },
-	{ id: 2, title: 'POST TWO' },
-	{ id: 3, title: 'POST THREE' },
-];
-
-// get all posts
-app.get('/api/posts', (req, res) => {
-	const limit = parseInt(req.query.limit);
-
-	if (!isNaN(limit) && limit > 0) {
-		return res.status(200).json(posts.slice(0, limit));
-	}
-	res.status(200).json(posts);
-});
-
-// get a single post
-app.get('/api/posts/:id', (req, res) => {
-	const id = parseInt(req.params.id);
-	const post = posts.find((post) => post.id === id);
-	if (!post) {
-		return res.status(404).json({ message: `A post with the id ${id} was not found` });
-	}
-	res.status(200).json(post);
-});
+// routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
