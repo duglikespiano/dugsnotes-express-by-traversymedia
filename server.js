17e07a24ecd7a48e8cb3948bbf7a1461e9711815
SUHO PARK <duglikespiano@gmail.com>
2025-09-15 17:06:52 +0900

    MIDDLEWARE - APP LEVEL

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
 
 const app = express();
 const port = process.env.PORT || 1000;
@@ -10,6 +11,9 @@ const port = process.env.PORT || 1000;
 
 // body parser middleware
 app.use(express.json());
+
+// logger middleware
+app.use(logger);
 
 // https://stackoverflow.com/questions/29960764/what-does-extended-mean-in-express-4-0
 // https://stackoverflow.com/questions/29175465/body-parser-extended-option-qs-vs-querystring/29177740#29177740
