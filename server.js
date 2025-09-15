38b5255707e3582248442261c6ec01c6e355b54b
SUHO PARK <duglikespiano@gmail.com>
2025-09-15 16:23:53 +0900

    HANDLING MULTIPLE RESPONSES

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.get('/api/posts', (req, res) => {
 	const limit = parseInt(req.query.limit);
 
 	if (!isNaN(limit) && limit > 0) {
-		res.status(200).json(posts.slice(0, limit));
-	} else {
-		res.status(200).json(posts);
+		return res.status(200).json(posts.slice(0, limit));
 	}
+	res.status(200).json(posts);
 });
 
 // get a single post
@@ -28,10 +27,9 @@ app.get('/api/posts/:id', (req, res) => {
 	const id = parseInt(req.params.id);
 	const post = posts.find((post) => post.id === id);
 	if (!post) {
-		res.status(404).json({ message: `A post with the id ${id} was not found` });
-	} else {
-		res.status(200).json(post);
+		return res.status(404).json({ message: `A post with the id ${id} was not found` });
 	}
+	res.status(200).json(post);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
