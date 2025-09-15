d436a00b39d5f73c844f763577a2192973c0395f
SUHO PARK <duglikespiano@gmail.com>
2025-09-15 16:02:24 +0900

    DEALING WITH DOTENV

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 1000;
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
