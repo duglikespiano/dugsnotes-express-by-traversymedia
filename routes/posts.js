4687e21b0ec460435b5e8db609aa861d17297bf2
SUHO PARK <duglikespiano@gmail.com>
2025-09-15 16:43:42 +0900

    PUT REQUEST

--- routes/posts.js
+++ routes/posts.js
@@ -40,4 +40,15 @@ router.post('/', (req, res) => {
 	res.status(201).json(posts);
 });
 
+// update a new post
+router.put('/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const post = posts.find((post) => post.id === id);
+	if (!post) {
+		return res.status(404).json({ message: `A post with the id ${id} was not found` });
+	}
+	post.title = req.body.title;
+	res.status(200).json(posts);
+});
+
 export default router;
